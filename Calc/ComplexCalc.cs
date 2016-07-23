7fef80406fe1f57fd2bb520a9ee852ff21909f47
bboyifeel <bboyifeel@gmail.com>
2016-07-23 16:06:25 +0300

    very basic translate from x y to modz argz

--- Calc/ComplexCalc.cs
+++ Calc/ComplexCalc.cs
@@ -15,14 +15,54 @@ namespace Calc
     [Activity(Label = "ComplexCalc")]
     public class ComplexCalc : Activity
     {
-        EditText aEditText;
-        EditText bEditText;
+        EditText xEditText;
+        EditText yEditText;
+        EditText modZEditText;
+        EditText argZEditText;
+        TextView test;
+
+        decimal x;
+        decimal y;
+        decimal modZ;
+        decimal argZ;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.ComplexCalc);
-            aEditText = FindViewById<EditText>(Resource.Id.aEditText);
-            bEditText = FindViewById<EditText>(Resource.Id.bEditText);
+            xEditText = FindViewById<EditText>(Resource.Id.xEditText);
+            yEditText = FindViewById<EditText>(Resource.Id.yEditText);
+            modZEditText = FindViewById<EditText>(Resource.Id.modZEditText);
+            argZEditText = FindViewById<EditText>(Resource.Id.argZbEditText);
+            test = FindViewById<TextView>(Resource.Id.test);
+
+            xEditText.KeyPress += onClick;
+            yEditText.KeyPress += onClick;
+        }
+        public void onClick(object sender, View.KeyEventArgs e)
+        {
+            if (e.KeyCode == Keycode.Enter)
+            {
+                string tmpA = "0";
+                string tmpB = "0";
+                if (xEditText.Text != "")
+                {
+                    tmpA = xEditText.Text;
+                }
+                if (yEditText.Text != "")
+                {
+                    tmpB = yEditText.Text;
+                }
+                x = decimal.Parse(tmpA);
+                y = decimal.Parse(tmpB);
+                modZ = (decimal)Math.Sqrt((double)(x * x + y * y));
+                argZ = (decimal)(Math.Atan2((double)y, (double)x) * (180 / Math.PI));
+                modZEditText.Text = modZ.ToString();
+                argZEditText.Text = argZ.ToString();
+            }
+            else
+            {
+                e.Handled = false;
+            }
         }
     }
 }
--- Calc/Resources/Resource.Designer.cs
+++ Calc/Resources/Resource.Designer.cs
@@ -63,14 +63,26 @@ namespace Calc
 			// aapt resource value: 0x7f050000
 			public const int Head = 2131034112;
 			
-			// aapt resource value: 0x7f050002
-			public const int aEditText = 2131034114;
+			// aapt resource value: 0x7f050004
+			public const int angle = 2131034116;
 			
-			// aapt resource value: 0x7f050003
-			public const int bEditText = 2131034115;
+			// aapt resource value: 0x7f050006
+			public const int argZbEditText = 2131034118;
+			
+			// aapt resource value: 0x7f050005
+			public const int modZEditText = 2131034117;
 			
 			// aapt resource value: 0x7f050001
 			public const int plusI = 2131034113;
+			
+			// aapt resource value: 0x7f050007
+			public const int test = 2131034119;
+			
+			// aapt resource value: 0x7f050002
+			public const int xEditText = 2131034114;
+			
+			// aapt resource value: 0x7f050003
+			public const int yEditText = 2131034115;
 			
 			static Id()
 			{
